2e8dce5ada8b6a5960d81b5c2e1bde2781ad1e11
Aielita <tungatarova04.10.2012@gmail.com>
2026-09-01 15:20:44 +0600

    Айэлита,про

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import AielitaPage from './pages/AielitaPage'
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path='/nur'/>
         <Route path='/med'/>
-        <Route path='/aie'/>
+        <Route path='/aie' element={<AielitaPage/>}/>
         <Route path='/sam'/>
         <Route path='/ami'/>
         <Route path='/bai'/>
---
+++ src/pages/AielitaPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import css from '../styles/AielitaPage.module.css'
+
+function AielitaPage() {
+  return (
+    <div className={css.box}>
+        <style>
+@import url('https://fonts.googleapis.com/css2?family=Mea+Culpa&display=swap');
+</style>
+        <h1>Привет, я Айэлита </h1>
+         <h3>Меня зовут Айэлита. Мне 14 лет, я живу в Кыргызстане. <br /> Я целеустремлённая, умная и всегда стараюсь добиваться своих целей.  Также я хожу на IT-курсы и <br /> на курсы английского языка. В своё свободное время я люблю слушать музыку и рисовать.</h3>
+    </div>
+  )
+}
+
+export default AielitaPage
